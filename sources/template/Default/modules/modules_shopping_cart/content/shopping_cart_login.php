4fb33b75dd8cbf7993dad06e1c599ba934415d3a
ClicShopping - e-commerce Solution B2B B2C <39338579+ClicShopping@users.noreply.github.com>
2019-04-23 09:49:29 -0400

    fix newsletter dropdown

--- sources/template/Default/modules/modules_shopping_cart/content/shopping_cart_login.php
+++ sources/template/Default/modules/modules_shopping_cart/content/shopping_cart_login.php
@@ -12,45 +12,32 @@
   use ClicShopping\OM\HTML;
   use ClicShopping\OM\CLICSHOPPING;
 ?>
-<div class="<?php echo $position; ?> col-md-<?php echo $content_width; ?>">
+<div class="col-md-<?php echo $content_width; ?> <?php echo $position; ?>">
   <div class="separator"></div>
   <div class="hr"></div>
   <?php echo $form; ?>
     <div class="col-md-12">
       <div class="separator"></div>
       <div class="ModulesShoppingCartLoginHeadingTitle"><h1><?php echo CLICSHOPPING::getDef('module_shopping_cart_login_title'); ?></h1></div>
-      <div class="col-md-5" style="padding-top:10px;">
-        <div class="input-group col-md-12">
-          <label for="InputEmail" class="sr-only"><?php echo CLICSHOPPING::getDef('module_shopping_cart_login_entry_email_address'); ?></label>
-          <span class="input-group-addon" id="InputEmail"><?php echo CLICSHOPPING::getDef('module_shopping_cart_login_entry_email_address'); ?></span>
-          <?php echo HTML::inputField('email_address', null, 'required aria-required="true" id="InputEmail" aria-describedby="' . CLICSHOPPING::getDef('module_shopping_cart_login_entry_email_address') . '" placeholder="' . CLICSHOPPING::getDef('module_shopping_cart_login_entry_email_address') . '"', 'email'); ?>
-        </div>
-        <div class="separator"></div>
+      <div class="separator"></div>
+      <div class="input-group col-md-12">
+        <label for="InputEmail" class="sr-only"><?php echo CLICSHOPPING::getDef('module_shopping_cart_login_entry_email_address'); ?>&nbsp;&nbsp;</label>
+        <span class="input-group-addon" id="InputEmail"><?php echo CLICSHOPPING::getDef('module_shopping_cart_login_entry_email_address'); ?>&nbsp;&nbsp;</span>
+        <?php echo HTML::inputField('email_address', null, 'required aria-required="true" id="InputEmail" aria-describedby="' . CLICSHOPPING::getDef('module_shopping_cart_login_entry_email_address') . '" placeholder="' . CLICSHOPPING::getDef('module_shopping_cart_login_entry_email_address') . '"', 'email'); ?>
       </div>
       <div class="separator"></div>
-      <div class="col-md-5">
-        <div class="input-group col-md-12">
-          <label for="inputPassword" class="sr-only"><?php echo CLICSHOPPING::getDef('module_shopping_cart_login_entry_password'); ?></label>
-          <span class="input-group-addon" id="inputPassword"><?php echo CLICSHOPPING::getDef('module_shopping_cart_login_entry_password'); ?></span>
-          <?php echo HTML::inputField('password', null, 'required aria-required="true" id="inputPassword" aria-describedby="' . CLICSHOPPING::getDef('module_shopping_cart_login_entry_password') . '" placeholder="' . CLICSHOPPING::getDef('module_shopping_cart_login_entry_password') . '"', 'password'); ?>
-        </div>
+
+      <div class="input-group col-md-12">
+        <label for="inputPassword" class="sr-only"><?php echo CLICSHOPPING::getDef('module_shopping_cart_login_entry_password'); ?>&nbsp;&nbsp;</label>
+        <span class="input-group-addon" id="inputPassword"><?php echo CLICSHOPPING::getDef('module_shopping_cart_login_entry_password'); ?>&nbsp;&nbsp;</span>
+        <?php echo HTML::inputField('password', null, 'required aria-required="true" id="inputPassword" aria-describedby="' . CLICSHOPPING::getDef('module_shopping_cart_login_entry_password') . '" placeholder="' . CLICSHOPPING::getDef('module_shopping_cart_login_entry_password') . '"', 'password'); ?>
       </div>
-      <div class="col-md-12 control-group">
-        <div class="controls">
-          <div class="buttonSet float-md-right"><?php echo HTML::button(CLICSHOPPING::getDef('module_shopping_cart_login_image_button_login'), null, null, 'success'); ?></div>
-        </div>
-      </div>
-    </div>
-    <?php echo $endform; ?>
-<!--
-    <div>
-      <div class="col-md-12">
-        <span class="ModulesShoppingCartLogin"><?php echo HTML::link(CLICSHOPPING::link(null, 'Account&LogIn'), CLICSHOPPING::getDef('module_shopping_cart_login_create_account')); ?></span>
-      </div>
-      <div class="col-md-12">
-        <span class="ModulesShoppingCartLoginpassword"><?php echo HTML::link(CLICSHOPPING::link(null, 'Account&PasswordForgotten'), CLICSHOPPING::getDef('module_shopping_cart_login_text_password_forgotten')); ?></span>
+
+
+      <div class="controls">
+        <div class="buttonSet float-md-right test-md-right"><?php echo HTML::button(CLICSHOPPING::getDef('module_shopping_cart_login_image_button_login'), null, null, 'success'); ?></div>
       </div>
     </div>
--->
+  <?php echo $endform; ?>
 </div>
-<div class="clearfix"></div>
+
