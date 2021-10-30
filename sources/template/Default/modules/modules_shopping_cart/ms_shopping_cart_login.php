d72472ac6b3dfb2bc7c6bdbb1397202dca222fe5
ClicShopping <admin@clicshopping.org>
2021-10-30 11:49:26 +0200

    update

--- sources/template/Default/modules/modules_shopping_cart/ms_shopping_cart_login.php
+++ sources/template/Default/modules/modules_shopping_cart/ms_shopping_cart_login.php
@@ -17,8 +17,8 @@
   class ms_shopping_cart_login {
     public string $code;
     public string $group;
-    public string $title;
-    public string $description;
+    public $title;
+    public $description;
     public ?int $sort_order = 0;
     public bool $enabled = false;
 
